7333a26f3897c223cc0c50f9302e95db321e5c91
Juan Collantes <juan@collantes.ec>
2024-03-20 12:51:41 -0500

    terminado

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,25 @@ import * as joi from 'joi';
 interface EnvVarsOptions {
   PORT: number;
   DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchemaValidator = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    //se valida que sea un array y que cada elemento sea un string.
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchemaValidator.validate(process.env);
+//const { error, value } = envsSchemaValidator.validate(process.env);
+const { error, value } = envsSchemaValidator.validate({
+  ...process.env,
+  //Para crear un array de nats servers basado en la variable de entorno NATS_SERVERS='nats://localhost:4222,nats://localhost:4223'
+  NATS_SERVERS: process.env.NATS_SERVERS.split(','),
+});
 
 if (error) {
   throw new Error(`Config envs validation error: ${error.message}`);
@@ -23,4 +32,5 @@ const envVars: EnvVarsOptions = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natServers: envVars.NATS_SERVERS,
 };
--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,12 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      //transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port,
+        //port: envs.port,
+        //servers: [`nats://localhost:4222`],
+        servers: envs.natServers,
       },
     },
   );
